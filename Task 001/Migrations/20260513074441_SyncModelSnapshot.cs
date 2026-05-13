dec9a85bacdc7cecfcaed5bf8c316351751649db
Mustafa Taher <mustafa.taher.abuelnasr@gmail.com>
2026-05-13 10:45:43 +0300

    role

---
+++ Task 001/Migrations/20260513074441_SyncModelSnapshot.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Task_001.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelSnapshot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "8595a1e5-61f6-419e-ab21-96f4f61afb47");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "ddbb6ddc-a0a3-4056-8bdb-6a80ffa208b8");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "4318f7a9-bb59-4220-b045-006d70104d93");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "f9667fbf-236d-4259-938c-ab17dae53011");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "2b33c974-16bc-44e3-aea6-0055bff3f23a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "3279ca46-f61a-4c51-b4eb-37770f15db6b");
+        }
+    }
+}
